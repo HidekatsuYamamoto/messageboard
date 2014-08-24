c8bea3eeb0f3067e9658b1aa1606c267b25a03ac
BadBoy <yokan@dk9.so-net.ne.jp>
2014-08-24 21:01:51 +0900

    Nothing i say to you.

---
+++ log_recording_class.php
@@ -0,0 +1,34 @@
+<?php
+    class log_recording_class{
+        const file = 'access_log.txt';
+        
+        // コンストラクタ
+        function __construct(){
+        }
+        
+        // 追加メソッド
+        function writeALogDown($textForWriteDown) {
+            $creationData = date('Y-m-d H-i-s');
+
+            // IPアドレスを取得して変数にセットする
+            $ipAddress = $_SERVER["REMOTE_ADDR"];
+
+            $GeneratedText = $creationData . '<divide>-' . $ipAddress . '-<divide>[' . $textForWriteDown . ']' ."\n";
+            return (file_put_contents(Self::file, $GeneratedText, FILE_APPEND | LOCK_EX));
+        }
+        
+        // method for reading
+        function readAllRecord() {
+            $data_array = file(Self::file);
+            if( $data_array === false ) {
+                writeALogDown("readAllRecord() is wrong.");
+                return $data_array = array();
+            }
+            return $data_array;
+        }
+        
+        // デストラクタ
+        function __destruct(){
+        }
+    }
+?>
